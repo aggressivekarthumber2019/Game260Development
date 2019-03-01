ad23dfdf07211350e4d7c6d542249f12f8576d16
Mina Infante <m.infante@bravozulu.ca>
2019-02-28 23:22:16 -0500

    Another Mod
    SpinOutMod- Added for item hits

---
+++ Resources/VehicleMovement/Source/VehicleMovement/Items/Mods/SpinOutMod.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawnStatMod.h"
+#include "SpinOutMod.generated.h"
+
+/**
+ * 
+ */
+UCLASS(editinlinenew)
+class VEHICLEMOVEMENT_API USpinOutMod : public UPawnStatMod
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		/** \brief	The amount of Speed after being Spun out */
+		float PostSpinOutSpeed;
+
+	virtual void EnableMod_Implementation(UCarStat* StatType) override;
+
+
+	virtual void DisableMod_Implementation(UCarStat* StatType) override;
+};
