e520a3a6bcd8dc908f43e227a6c31f4fe491f661
Mike Wells <mwcwells@gmaill.com>
2019-04-12 16:51:18 -0400

    Updates to game, trying to reduce errors

--- Resources/VehicleMovementProject/Source/VehicleMovement/FSM/PawnStatComponent.cpp
+++ Resources/VehicleMovementProject/Source/VehicleMovement/FSM/PawnStatComponent.cpp
@@ -47,8 +47,8 @@ void UPawnStatComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAc
 	{
 		UStatModTracker* CurrentMod = &Entry.Value;
 
-		// Subtract time
-		if (CurrentMod->MMod->MMaxTimeMS != 0.f)
+		// Check that the current mod is valid first then Subtract time
+		if (CurrentMod && CurrentMod->MMod->MMaxTimeMS != 0.f)
 		{
 			// Subtract time
 			CurrentMod->MModTimeRemainMS -= DeltaTime * 1000.f;
@@ -115,8 +115,8 @@ void UPawnStatComponent::DisableMod(UPawnStatMod* Mod)
 {
 	// Mod id
 	FName ObjectID = Mod->StaticClass()->GetDefaultObjectName();
-	// Check if we have that mod
-	if (MStatModifiers.Contains(ObjectID))
+	// Check if the ObjectID is valid and Check if we have that mod
+	if (ObjectID.IsValid() && MStatModifiers.Contains(ObjectID))
 	{
 		UStatModTracker* CurrentMod = &MStatModifiers[ObjectID];
 
@@ -131,7 +131,11 @@ void UPawnStatComponent::DisableMod(UPawnStatMod* Mod)
 		}
 	}
 
-	Mod->DisableMod(this, Cast<AKartVehiclePawn>(GetOwner()));
+	//Check if valid before using the mod
+	if (Mod)
+	{
+		Mod->DisableMod(this, Cast<AKartVehiclePawn>(GetOwner()));
+	}
 }
 
 void UPawnStatComponent::AddItem(UPawnStatMod* Mod)
